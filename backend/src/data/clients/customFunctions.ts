feb8bf00400c94db5c0809d619a5e0ad24d0e887
Lucas Santos <lhs.santoss@gmail.com>
2022-02-16 20:40:04 +0100

    fix(functions): fix custom functions handler

--- backend/src/data/clients/customFunctions.ts
+++ backend/src/data/clients/customFunctions.ts
@@ -1,20 +1,20 @@
 'use strict'
 
-const customValidation = require('../helpers/example')
-
 // eslint-disable-next-line no-unused-vars,require-await
 module.exports = async (server, { hdbCore }) => {
 
   server.route({
     url: '/ships',
     method: 'GET',
-    handler: (request) => {
+    preParsing: (request, _, done) => {
       request.body = {
         operation: 'sql',
         sql: 'SELECT events FROM ship_manager.ships WHERE search_json("deletedAt", state) IS NULL'
       }
-      return hdbCore.requestWithoutAuthentication(request)
-    }
+      done()
+    },
+    preValidation: hdbCore.preValidation,
+    handler: hdbCore.request
   })
 
   server.route({
