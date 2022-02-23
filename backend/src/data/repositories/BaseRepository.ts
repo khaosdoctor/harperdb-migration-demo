20c128af2b4ee9cc78a1dce06a23112142fec578
Lucas Santos <lhs.santoss@gmail.com>
2022-02-23 21:57:42 +0100

    refactor: refactor repository
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- backend/src/data/repositories/BaseRepository.ts
+++ backend/src/data/repositories/BaseRepository.ts
@@ -1,44 +1,42 @@
-import { HarperDBClient } from '../clients/HarperDBClient'
-import { ObjectId } from 'mongodb'
 import { IEventEntity } from '@irontitan/paradox'
 import { IEntityConstructor } from '@irontitan/paradox/dist/interfaces/IEntityConstructor'
 import cloneDeep from 'lodash.clonedeep'
-
+import { ObjectId } from 'mongodb'
+import { HarperDBClient } from '../clients/HarperDBClient'
 export class BaseRepository<Entity extends IEventEntity> {
   protected database: HarperDBClient
   protected tableName: string
   protected entity: IEntityConstructor<Entity>
+
   constructor (client: HarperDBClient, tableName: string, entity: IEntityConstructor<Entity>) {
-    this.database = client
     this.tableName = tableName
+    this.database = client
     this.entity = entity
   }
 
   async findById (id: string | ObjectId): Promise<Entity | null> {
     if (!ObjectId.isValid(id)) return null
-    const oid = new ObjectId(id)
 
-    const document = await this.database.NoSQLFindByID<Entity>(oid.toString(), this.tableName, ['state', 'events'])
+    const document = await this.database.NoSQLFindByID<Entity>(id.toString(), this.tableName, ['state', 'events'])
     if (!document) return null
-
     return new this.entity().setPersistedEvents(document.events)
   }
 
   async save (entity: Entity): Promise<Entity> {
     const localEntity = cloneDeep(entity)
     const document = {
-      _id: entity.id,
+      _id: localEntity.id,
       state: localEntity.state,
       events: localEntity.persistedEvents.concat(localEntity.pendingEvents)
     }
     const result = await this.database.NoSQLUpsert([document], this.tableName)
-    console.log(result)
     if (!result.upserted_hashes.includes(document._id.toString())) throw new Error(result.message)
-    return entity.confirmEvents()
+
+    return localEntity.confirmEvents()
   }
 
   async getAll (): Promise<Entity[]> {
     const documents = await this.database.SQLFindAll<{ events: Entity['events'] }>(this.tableName)
-    return documents.map((document) => new this.entity().setPersistedEvents(document.events))
+    return documents.map(document => new this.entity().setPersistedEvents(document.events))
   }
 }
